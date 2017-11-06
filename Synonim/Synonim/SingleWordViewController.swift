39e41c2d3937b20da8038bdcd1483831818f1afe
hg1zadr <hg1zadr@gmail.com>
2017-11-06 17:31:42 +0600

    Ready

--- Synonim/Synonim/SingleWordViewController.swift
+++ Synonim/Synonim/SingleWordViewController.swift
@@ -10,27 +10,20 @@ import UIKit
 
 class SingleWordViewController: UIViewController {
 
-    @IBOutlet weak var wordLabel: UILabel!
+
+    @IBOutlet weak var synonymLabel: UILabel!
+    
+    var word : String!
+    var index : Int!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        synonymLabel.text = word
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Synonim/Synonim/ViewController.swift
+++ Synonim/Synonim/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var viewPlace: UIView!
     
+    var words : [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -30,7 +32,84 @@ class ViewController: UIViewController {
         let url = "http://api.wordnik.com:80/v4/word.json/\(text)/relatedWords?useCanonical=false&relationshipTypes=synonym&limitPerRelationshipType=10&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5"
         print(url)
         
-        URLSession.shared.dataTask(with: URL(sting: url)!, completionHandler: <#T##(Data?, URLResponse?, Error?) -> Void#>)
+        URLSession.shared.dataTask(with: URL(string: url)!) { (data, response, error) in
+            if(error != nil){
+                print(error?.localizedDescription)
+            }else{
+                print(String(data: data!, encoding: String.Encoding.utf8))
+                
+                let json = try! JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! [[String: AnyObject]]
+                
+                if(json.count > 0){
+                    let item = json[0]
+                    
+                    if let words = item["words"] as? [String] {
+                        DispatchQueue.main.async {
+                            self.displayWords(words: words)
+                        }
+                    }
+                }
+            }
+        }.resume()
+    }
+    
+    func displayWords(words: [String]) {
+        self.words = words
+        
+        if(words.count > 0) {
+            
+            let singleWordVC = self.storyboard?.instantiateViewController(withIdentifier: "SingleWordVC") as! SingleWordViewController
+            
+            singleWordVC.word = words[0]
+            singleWordVC.index = 0
+            
+            let pageVC = UIPageViewController()
+            
+            pageVC.setViewControllers([singleWordVC], direction: .forward, animated: true, completion: nil)
+            pageVC.view.frame = self.viewPlace.frame
+            
+            self.addChildViewController(pageVC)
+            self.view.addSubview(pageVC.view)
+            pageVC.didMove(toParentViewController: self)
+            
+            pageVC.dataSource = self
+            
+        }
+    }
+    
+}
+
+extension ViewController : UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        let index = (viewController as! SingleWordViewController).index - 1
+        
+        if(index < 0) {
+            return nil
+        }
+        
+        let singleWordVC = self.storyboard?.instantiateViewController(withIdentifier: "SingleWordVC") as! SingleWordViewController
+        
+        singleWordVC.word = words[index]
+        singleWordVC.index = index
+        
+        return singleWordVC
+        
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        let index = (viewController as! SingleWordViewController).index + 1
+        
+        if(index >= words.count) {
+            return nil
+        }
+        
+        let singleWordVC = self.storyboard?.instantiateViewController(withIdentifier: "SingleWordVC") as! SingleWordViewController
+        
+        singleWordVC.word = words[index]
+        singleWordVC.index = index
+        
+        return singleWordVC
     }
 }
 
@@ -38,5 +117,6 @@ extension ViewController : UISearchBarDelegate{
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         let text = searchBar.text!
         self.searchSynonyms(text: text)
+        searchBar.text = ""
     }
 }
